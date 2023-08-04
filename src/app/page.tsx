2e2d5bbe5cbea9ff70be6987feb6339f0ddd522a
Kave McDaniel <kave.mdi@gmail.com>
2023-08-04 20:16:56 +0330

    Added Services section to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import { CTA } from "components/CTA";
-import { FAQ } from "components/FAQ";
-import { Features } from "components/Features";
-import { DarkHero } from "components/Hero/DarkHero";
-import { JoinCommunity } from "components/JoinCommunity";
-import { Testimonial } from "components/Testimonial";
+import {FAQ} from "components/FAQ";
+import {Features} from "components/Features";
+import {DarkHero} from "components/Hero/DarkHero";
+import {JoinCommunity} from "components/JoinCommunity";
+import {Testimonial} from "components/Testimonial";
+import {OfferCta} from 'components/OfferCTA/OfferCTA';
+import {TestCta} from 'components/TestCta/TestCta';
+import {Services} from 'components/Hero/Services';
 
 export default function Home() {
   return (
     <main id="page-content" className="flex flex-auto flex-col max-w-full">
       <DarkHero />
       <Features />
+      <Services/>
+      <OfferCta />
       <Testimonial />
       <FAQ />
-      <CTA />
       <JoinCommunity />
+      <TestCta />
     </main>
   );
 }
