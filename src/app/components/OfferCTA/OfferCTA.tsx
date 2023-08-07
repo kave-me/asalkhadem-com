2df42f7f7223103b69df108c0b1f184167ba9183
Kave McDaniel <kave.mdi@gmail.com>
2023-08-08 03:06:32 +0330

    Add what is existentialism section

--- src/app/components/OfferCTA/OfferCTA.tsx
+++ src/app/components/OfferCTA/OfferCTA.tsx
@@ -1,51 +1,43 @@
-export function OfferCta() {
-	return (
-		<div className="bg-gray-100 overflow-hidden">
-			<div className="container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
-				<div className="relative">
-					<div
-						className="pattern-dots-lg text-gray-300 absolute top-0 right-0 w-32 h-32 lg:w-48 lg:h-48 transform translate-x-12 -translate-y-16"></div>
-					<div
-						className="pattern-dots-lg text-gray-300 absolute bottom-0 left-0 w-32 h-32 lg:w-48 lg:h-48 transform -translate-x-12 translate-y-16"></div>
-					<div className="p-10 lg:py-12 lg:px-16 bg-white rounded shadow text-center relative">
-						<div className="space-y-10">
-							<div className="text-center">
-								<h2 className="text-3xl md:text-4xl font-extrabold mb-4">
-									هنوز اطمینان ندارید؟{" "}
-									<span className="text-indigo-600">تردید دارید؟</span>
-								</h2>
-								<h3 className="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600">
-									دکتر عسل خادم دانش پژوه دکتری تخصصی روانشناسی سلامت روانشناس و
-									روان درمانگر روان درمانی فردی: - اضطراب و افسردگی - احساس پوچی
-									و بی هدفی (خلاء وجودی) رواندرمانی بین فردی و رابطه گروه درمانی
-									اگزیستانسیال
-								</h3>
-							</div>
+function VideoTutorial() {
+	return (<iframe
+		width="640"
+		height="360"
+		sandbox="allow-scripts allow-same-origin"
+		src="https://www.aparat.com/video/video/embed/videohash/t9UBw/vt/frame?isamp"
+		title="اگزیستانسیالیسم چیست؟"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowFullScreen></iframe>);
+}
 
-							<div className="flex flex-col sm:flex-row sm:items-center sm:justify-center gap-4">
+export function OfferCta() {
+	return (<div className="bg-gray-100 overflow-hidden">
+		<div className="container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
+			<div className="relative">
+				<div
+					className="pattern-dots-lg text-gray-300 absolute top-0 right-0 w-32 h-32 lg:w-48 lg:h-48 transform translate-x-12 -translate-y-16"></div>
+				<div
+					className="pattern-dots-lg text-gray-300 absolute bottom-0 left-0 w-32 h-32 lg:w-48 lg:h-48 transform -translate-x-12 translate-y-16"></div>
+				<div className="p-10 lg:py-12 lg:px-16 bg-white rounded shadow text-center relative">
+					<div className="space-y-10">
+						<div className="text-center">
+							<h2 className="text-3xl md:text-4xl font-extrabold mb-4">درمان {" "}<span
+								className="text-indigo-600">اگزیستانسیال</span>{" "}چیست؟
+							</h2>
+							<h3 className="text-lg md:text-lg md:leading-relaxed font-medium text-gray-600 py-8">این درمان به شما کمک
+								می‌کند تا بتوانید در مواجهه با انتخابات مهم و تصمیمات زندگی خود، تحمل اضطراب و استرس را تجربه کنید و با
+								شرایط پیچیده و آشفته زندگی، به خوبی سازگار شوید.
+							</h3>
+							{/* Space */}
+							<div className="py-8 mx-auto flex justify-center">
+								<VideoTutorial/>
+							</div>
+							<div className="flex flex-col sm:flex-row sm:items-center sm:justify-center gap-4 mt-8">
 								<a
-									href="#"
-									className="inline-flex justify-center items-center border font-semibold focus:outline-none px-6 py-4 leading-6 rounded border-indigo-700 bg-indigo-700 text-white hover:text-white hover:bg-indigo-800 hover:border-indigo-800 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-700 active:border-indigo-700 lg:w-1/4 xs:w-1/3 "
-								>
-									<span className={"mx-2"}>رزرو جلسه</span>
-									<svg
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-										className="opacity-50 hi-solid hi-arrow-left rotate-180 inline-block w-5 h-5"
-									>
-										<path
-											fillRule="evenodd"
-											d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-											clipRule="evenodd"
-										></path>
-									</svg>
-								</a>
-								<a
-									href="#"
+									target="_blank"
+									href="https://www.aparat.com/v/t9UBw"
 									className="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-6 py-4 leading-6 rounded border-gray-200 bg-gray-200 text-gray-700 hover:text-gray-700 hover:bg-gray-300 hover:border-gray-300 focus:ring focus:ring-gray-500 focus:ring-opacity-25 active:bg-gray-200 active:border-gray-200 lg:w-1/4 xs:w-1/3"
 								>
-									<span className={" mx-2"}>اطلاعات بیشتر</span>
+									<span className={"text-gray-500 mx-2"}>اگزیستانسیال در سه دقیقه</span>
 									<svg
 										fill="currentColor"
 										viewBox="0 0 20 20"
@@ -60,10 +52,40 @@ export function OfferCta() {
 									</svg>
 								</a>
 							</div>
+							{/* divider */}
+							<div className="border-b border-gray-200 my-8 lg:my-12"></div>
+							<h3 className="text-3xl md:text-2xl font-extrabold mb-4">درمان {" "}<span
+								className="text-indigo-600">اگزیستانسیال</span>{" "}چگونه به من کمک می کند؟
+							</h3>
+							<h3 className="text-lg md:text-lg md:leading-relaxed font-medium text-gray-600">می‌توان گفت که این روش
+								مانند بازکردن درهای نور به درون ذهن شماست. در مسیر یادگیری با نوآوری‌ها و ابزارهای جدیدی برای تغییر و
+								رشد آشنا می‌شوید و رازهای نهفته درون‌تان را کشف می‌کنید. درمان اگزیستانسیال، چشم‌اندازی جدید به شما و
+								زندگی‌تان می‌بخشد.</h3>
+						</div>
+						<div className="flex flex-col sm:flex-row sm:items-center sm:justify-center gap-4">
+							<a
+								target="_blank"
+								href="https://pezeshkekhoob.com/doctor/51213-asal-khadem"
+								className="inline-flex justify-center items-center border font-semibold focus:outline-none px-6 py-4 leading-6 rounded border-indigo-700 bg-indigo-700 text-white hover:text-white hover:bg-indigo-800 hover:border-indigo-800 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-700 active:border-indigo-700 lg:w-1/4 xs:w-1/3"
+							>
+								<span className={"mx-2  animate-pulse"}>رزرو جلسه</span>
+								<svg
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg"
+									className="opacity-50 hi-solid hi-arrow-left rotate-180 inline-block w-5 h-5 "
+								>
+									<path
+										fillRule="evenodd"
+										d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+										clipRule="evenodd"
+									></path>
+								</svg>
+							</a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	);
+	</div>);
 }
